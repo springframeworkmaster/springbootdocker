b0dc484536cae71bfee1c747fad1e3668c4fd1f3
Sai krishna <sskrishnas@gmail.com>
2018-04-11 22:00:39 -0700

    initial commit

---
+++ src/main/java/com/springframeworkmaster/docker/springbootdocker/SpringbootdockerApplication.java
@@ -0,0 +1,12 @@
+package com.springframeworkmaster.docker.springbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootdockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootdockerApplication.class, args);
+    }
+}
---
+++ src/main/java/com/springframeworkmaster/docker/springbootdocker/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.springframeworkmaster.docker.springbootdocker.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "Home Controller";
+    }
+}
